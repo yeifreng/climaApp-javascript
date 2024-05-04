6be327141dc670211581f3dacca6a4314f840803
Yeifreng <yeifrenjose@gmail.com>
2024-05-04 12:19:28 -0500

    Creando card independiente y mostrando imagen

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let urlBase = 'https://api.openweathermap.org/data/2.5/weather'
 
 let difKelvin = 273.15
 
+
 document.getElementById('botonBusqueda').addEventListener('click', buscar)
 
 
@@ -20,27 +21,42 @@ function datosClima(ciudad){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`)
     .then(data => data.json())
     .then(data => mostrarDatos(data))
-    .catch( error => console.error(error) )
+    .catch( error => console.error(error))
 
 }
 
 function mostrarDatos(data){
+
     const divDatosClima = document.getElementById('datosClima')
+    divDatosClima.classList.add('contenedor-dos')
     divDatosClima.innerHTML = ''
     const temperatura = data.main.temp
+    const temperaturaInt = parseInt(temperatura)
+    const temperaturaMenosKelvin = temperaturaInt - difKelvin
+    const temperaturaFinal = Math.round(temperaturaMenosKelvin)
+    const humedad = data.main.humidity
     const ciudadNombre = data.name
     const descripcion = data.weather[0].description
+    const icono = data.weather[0].icon
 
     const ciudadTitulo = document.createElement('h2')
     ciudadTitulo.textContent = ciudadNombre
 
     const temperaturaInfo = document.createElement('p')
-    temperaturaInfo.textContent = `la temperatura es: ${temperatura- difKelvin}°C`
+    temperaturaInfo.textContent = `Temperatura: ${temperaturaFinal}°C`
+
+    const humedadInfo = document.createElement('p')
+    humedadInfo.textContent = `Humedad: ${humedad}%`
+
+    const iconoInfo = document.createElement('img')
+    iconoInfo.src = `https://openweathermap.org/img/wn/${icono}@4x.png` 
 
     const descripcionInfo = document.createElement('p')
     descripcionInfo.textContent =  `La descripcion meteorologica es: ${descripcion}`
 
+    divDatosClima.appendChild(iconoInfo)
     divDatosClima.appendChild(ciudadTitulo)
     divDatosClima.appendChild(temperaturaInfo)
-    divDatosClima.appendChild(descripcionInfo)
+    //divDatosClima.appendChild(descripcionInfo)
+    divDatosClima.appendChild(humedadInfo)
 }
